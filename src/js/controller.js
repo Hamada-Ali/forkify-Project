d155c79fa34d562bda05ee5d68c770f806defc4f
Hamada Ali <hamadaahmed927@gmail.com>
2022-12-15 13:02:23 +0200

    third

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import addRecipeView from './views/addRecipeView.js';
+import Pagination from './views/paginationView.js';
 
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
@@ -39,7 +40,10 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.getSearchResultsPage());
+    resultsView.render(model.getSearchResultsPage(6));
+
+    // init pagination buttons
+    Pagination.render(model.state.search)
 
   } catch (err) {
     console.log(err);
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,64 @@
+// pagination View page
+
+import View from './View.js';
+
+// import icons from '../img/icons.svg'; // Parcel 1
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+import { Fraction } from 'fractional';
+
+class Pagination extends View {
+    _parentElement = document.querySelector('.pagination')
+
+    _generateMarkup() {
+        const numpages = Math.ceil(this._data.results.length /this._data.resultsPerPage);
+        console.log(numpages)
+
+    // page 1 and there is no other pages
+    if(this._data.page === 1 && numpages > 1) {
+        return `
+             <button class="btn--inline pagination__btn--next">
+                 <span>Page ${this._data.page + 1}</span>
+                 <svg class="search__icon">
+                    <use href="${icons}#icon-arrow-right"></use>
+                 </svg>
+          </button> 
+        `
+    }
+
+    // last page
+    if(this._data.page === numpages && numpages > 1) {
+        return `
+            <button class="btn--inline pagination__btn--prev">
+                <svg class="search__icon">
+                    <use href="${icons}#icon-arrow-left"></use>
+                </svg>
+                <span>Page ${this._data.page - 1}</span>
+            </button>
+        `
+    }
+
+    // other page
+    if(this._data.page < numpages) {
+        return `
+        <button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${this._data.page - 1}</span>
+        </button>
+        <button class="btn--inline pagination__btn--next">
+            <span>Page ${this._data.page + 1}</span>
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+        </button> 
+        `
+    }
+
+    // page 1 and there is no other pages
+    return ''
+
+    }
+}
+
+export default new Pagination()
